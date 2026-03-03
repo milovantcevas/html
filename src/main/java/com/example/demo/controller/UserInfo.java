5de022a26531272cccd6ae6435c5c4c97bcaddea
HHOOSSTT <milovanarsen@gmail.com>
2026-03-03 10:51:47 +0300

    03.03

---
+++ src/main/java/com/example/demo/controller/UserInfo.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserInfo {
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("firstname")
+    private String firstName;
+
+    @JsonProperty("lastname")
+    private String lastName;
+
+    public UserInfo() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserInfo(Long userId, String firstName, String lastName) {
+        this.id = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
